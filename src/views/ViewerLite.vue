a1f417e097e55a5fb8b37e2d46ae7b085b3fd04b
Spiridonov Maxim <taijased@gmail.com>
2020-03-10 20:39:01 +0400

    lite viewer

---
+++ src/views/ViewerLite.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+    .viewer-lite
+
+</template>
+
+
+<script>
+
+import { mapGetters, mapActions} from 'vuex';
+
+export default {
+
+}
+
+</script>
+
+<style lang="stylus" scoped>
+.viewer-lite 
+    width 100vh
+    height 100vh
+    overflow hidden
+    background $primary
+
+</style>
